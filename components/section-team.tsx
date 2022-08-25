944f0b2be6197b3f4f2cf279a6f326773fd71fa9
risacan <supikateriburu@gmail.com>
2022-08-25 22:54:09 +0900

    TEAM Sectionを変更しました！ (#1)
    * ✨ Add team section
    
    * 👩‍🦰 Add icon pic

--- components/section-team.tsx
+++ components/section-team.tsx
@@ -29,8 +29,8 @@ const members: Profile[] = [
   {
     name: "Risa",
     job: "Software Engineer",
-    pic: "placeholder",
-    twitter: "@",
+    pic: "/risa.jpeg",
+    twitter: "risacan_eth",
   },
   {
     name: "Miho",
@@ -69,26 +69,20 @@ const SectionTeam = ({ bgColor, isFading, title, contents }: Props) => {
           <div className='justify-center8 flex w-full flex-col items-center'>
             <div className='flex w-full flex-col flex-wrap items-center justify-between lg:flex-row lg:pl-4'>
               {members.map((member, index) => (
+
                 <div
                   className='mx-5 mb-10 flex flex-col  items-center justify-center'
                   key={index}
                 >
-                  <svg
-                    xmlns='http://www.w3.org/2000/svg'
-                    className='h-20 w-20'
-                    fill='none'
-                    viewBox='0 0 24 24'
-                    stroke='currentColor'
-                    strokeWidth='2'
-                  >
-                    <path
-                      strokeLinecap='round'
-                      strokeLinejoin='round'
-                      d='M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z'
-                    />
-                  </svg>
-                  <p className='text-sm'>{member.name}</p>
-                  <p className='text-sm'>{member.job}</p>
+                <img
+                  src={member.pic}
+                  width="100px"
+                  height="100px"
+                  alt={`${member.name}'s image`}
+                />
+                <p className='text-sm'>{member.name}</p>
+                <p className='text-sm'>{member.job}</p>
+                <p className="text-sm hover:underline"><a href={`https://twitter.com/${member.twitter}`} target="_blank">@{member.twitter}</a></p>
                 </div>
               ))}
             </div>
