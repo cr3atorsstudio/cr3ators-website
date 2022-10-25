48257ea710739eb2ba251aed1fa482bf575d4de2
mihoogura <miho@ryu.games>
2022-10-25 17:28:34 +0900

    Fix the api key

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,55 +3,53 @@ import Layout from "../components/layout";
 import Head from "next/head";
 import Navbar from "../components/navbar";
 
-import '@rainbow-me/rainbowkit/styles.css';
+import "@rainbow-me/rainbowkit/styles.css";
 import {
   getDefaultWallets,
   lightTheme,
-  RainbowKitProvider
-} from '@rainbow-me/rainbowkit';
-import {
-  chain,
-  configureChains,
-  createClient,
-  WagmiConfig,
-} from 'wagmi';
-import { alchemyProvider } from 'wagmi/providers/alchemy';
-import { publicProvider } from 'wagmi/providers/public';
+  RainbowKitProvider,
+} from "@rainbow-me/rainbowkit";
+import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
 
 export default function Index() {
   const { chains, provider } = configureChains(
     [chain.mainnet],
     [
-      alchemyProvider({ apiKey: process.env.ALCHEMY_ID }),
-      publicProvider()
+      alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_ID }),
+      publicProvider(),
     ]
   );
 
   const { connectors } = getDefaultWallets({
-    appName: 'Creators Studio',
-    chains
+    appName: "Creators Studio",
+    chains,
   });
 
   const wagmiClient = createClient({
     autoConnect: true,
     connectors,
-    provider
-  })
+    provider,
+  });
 
   return (
     <WagmiConfig client={wagmiClient}>
-      <RainbowKitProvider chains={chains} theme={lightTheme({
-        accentColor: `var(--bg-connect-wallet-btn)`,
-        borderRadius: 'large'
-      })}>
+      <RainbowKitProvider
+        chains={chains}
+        theme={lightTheme({
+          accentColor: `var(--bg-connect-wallet-btn)`,
+          borderRadius: "large",
+        })}
+      >
         <Layout>
           <Head>
             <title>Creators Studio</title>
-            <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link rel="preconnect" href="https://fonts.gstatic.com" />
+            <link rel='preconnect' href='https://fonts.googleapis.com' />
+            <link rel='preconnect' href='https://fonts.gstatic.com' />
             <link
-              href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&family=Roboto:ital,wght@0,300;0,400;1,400&display=swap"
-              rel="stylesheet"
+              href='https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&family=Roboto:ital,wght@0,300;0,400;1,400&display=swap'
+              rel='stylesheet'
             />
           </Head>
           <Navbar />
