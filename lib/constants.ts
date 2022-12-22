623312c6075ca5ae9c54c7bddd4989ba1ad2587f
riho.takagi <rihohearts@gmail.com>
2022-12-22 11:34:11 -0500

    Update contract address

--- lib/constants.ts
+++ lib/constants.ts
@@ -2,4 +2,4 @@ export const EXAMPLE_PATH = "blog-starter";
 export const CMS_NAME = "Markdown";
 export const HOME_OG_IMAGE_URL =
   "https://og-image.vercel.app/Next.js%20Blog%20Starter%20Example.png?theme=light&md=1&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg";
-export const CONTRACT_ADDRESS = "0xF6d4c167F10750e0964a2EDdB1F06BC664119259";
+export const CONTRACT_ADDRESS = "0xe619Cb6d9b22ACec9bF6458FE3EFef3A21E35835";
